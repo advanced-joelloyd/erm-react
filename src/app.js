423c9cf40eadacc4853f82ecf8f3a3323e582666
Joe Lloyd <joe.lloyd@advancedcomputersoftware.com>
2016-05-04 13:26:41 +0100

    Add simple client-side React app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Item = require('./components/Item.jsx');
+
+
+ReactDOM.render(<Item description="Pineapple" />, document.getElementById('content'));
+
